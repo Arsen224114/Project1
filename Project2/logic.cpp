d1a051ea13da87b918588a3291604fc0713c6549
Arsen224114 <ayarmalitski@gmail.com>
2025-05-16 18:34:56 +0300

    task02

---
+++ Project2/logic.cpp
@@ -0,0 +1,21 @@
+bool equals(int first, int second) {
+	if (second < 0) {
+		return false;
+	}
+
+	if (first < 0) {
+		first = -first;
+	}
+
+	if (first == second) {
+		return true;
+	}
+
+	if (first == 0 && second == 0) {
+		return false;
+	}
+
+	int digit = first % 10;
+	return equals(first / 10, second - digit);
+
+}
